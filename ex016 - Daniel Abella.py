9e876402e3d631d069f0d4f88a787e0c1d87560b
RyandroTavares <ryandrotavaresbr@gmail.com>
2022-04-10 08:59:51 -0300

    Questão-16

---
+++ ex016 - Daniel Abella.py
@@ -0,0 +1,20 @@
+# 16 Faça um programa que, dado um conjunto de N números, determine o menor valor, o maior valor e a soma dos valores.
+
+quantidade = int(input('Digite a quantidade de números: '))
+maior = menor = soma = contagem = 0
+print()
+
+for i in range(quantidade):
+    numero = int(input('Digite um número: '))
+    soma = soma + numero
+    contagem = contagem + 1
+    if contagem == 1:
+        maior = menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+
+print()
+print('A soma dos números da {}, o maior deles {} e o menor deles {}!'.format(soma, maior, menor))
