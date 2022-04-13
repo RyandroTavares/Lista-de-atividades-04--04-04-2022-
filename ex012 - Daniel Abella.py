6e1eb0aa3a43eed986f56328301327813fce836e
RyandroTavares <ryandrotavaresbr@gmail.com>
2022-04-13 20:38:52 -0300

    Questão-12

--- ex012 - Daniel Abella.py
+++ ex012 - Daniel Abella.py
@@ -1,8 +1,10 @@
 # 12 Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10. O usuário deve informar de qual numero ele deseja ver a tabuada.
-#A saída deve ser conforme o exemplo abaixo.
+# A saída deve ser conforme o exemplo abaixo.
 
-valor2 = 0
-valorfinal = 0
+valor2 = valorfinal = 0
+valorinicial = int(input('Digite o valor para ser multiplicado na tabuada: '))
+print()
+
 for valor2 in range(11):
-    valorfinal = 5 * valor2
-    print('{} x {} = {}'.format(5, valor2, valorfinal))
+    valorfinal = valorinicial * valor2
+    print('{} x {} = {}'.format(valorinicial, valor2, valorfinal))
